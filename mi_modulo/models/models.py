de102a32177e4bd05680569732d3fa6f3a3ee469
DiegoOrtegaElRincon <118464823+DiegoOrtegaElRincon@users.noreply.github.com>
2023-12-15 16:15:29 +0000

    Add files via upload

--- mi_modulo/models/models.py
+++ mi_modulo/models/models.py
@@ -8,7 +8,7 @@ class EmpresaContratista(models.Model):
     name = fields.Char(string='Nombre', required=True)
     direccion = fields.Char(string='Direccion', required=True)
     display_info = fields.Char(string='Display Info', compute='_compute_display_info')
-    proyectos_ids = fields.One2many('project.project', 'empresas_contratadoras', string='Proyectos')
+    proyectos_ids = fields.One2many('project.project', 'contratadora', string='Proyectos')
 
     @api.depends('name', 'direccion')
     def _compute_display_info(self):
@@ -16,10 +16,11 @@ class EmpresaContratista(models.Model):
             record.display_info = "{} - {}".format(record.name, record.direccion)
  
 class Proyecto(models.Model):
+    _name = 'project.project'
     _inherit = 'project.project'
 
     new_field = fields.Char(string='New Field')
-    empresas_contratadoras = fields.Many2one("empresas_contratista",string="Empresa Contratadora",inverse_name='proyectos_ids')
+    contratadora = fields.Many2one("empresas_contratista", string="Empresa Contratadora", inverse_name='proyectos_ids', required="True", ondelete="cascade")
     tareas_ids = fields.One2many('project.task', 'project_id', string='Tareas')
     total_tareas = fields.Integer(string='Total de Tareas', compute='_compute_total_tareas', store=True)
     
@@ -29,6 +30,7 @@ class Proyecto(models.Model):
             proyecto.total_tareas = len(proyecto.tareas_ids)
 
 class Tarea(models.Model):
+    _name = 'project.task'
     _inherit = 'project.task'
 
     proyecto_id = fields.Many2one('proyecto', string='Proyecto')
